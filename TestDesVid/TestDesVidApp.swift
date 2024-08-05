20cc98d19599b8850cb8d8ea70ed2d98824eb4fa
sonpt61 <sonpt61@fpt.com>
2024-08-05 15:47:35 +0700

    Initial Commit

---
+++ TestDesVid/TestDesVidApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDesVidApp.swift
+//  TestDesVid
+//
+//  Created by SonPT61 on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDesVidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
